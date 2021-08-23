f1280c9af81ee56db7453589c2f06dca33c375f7
mohamed magdy <mm88mm05@yahoo.com>
2021-08-23 09:23:45 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+// // var win ;
+// // function openWindow(){
+// //    win = window.open("childWindow.html" , "", "width= 150 , height=150")
+// // }
+
+// // function closeWindow(){
+// //     win.close()
+// // }
+
+// // function changeBg(){
+// //     win.document.bgColor ="red"
+// //     win.focus()
+// // }
+
+// // function moveByWindow(){
+// //     win.moveBy(50,50)
+// //     win.focus()
+// // }
+
+// // function moveToWindow(){
+// //     win.moveTo(50,50)
+// //     win.focus()
+// // }
+
+// var alertTime;
+
+// function startAlert(){
+// //   alertTime = setInterval(function(){
+// //        alert('alert fired')
+// //    },3000);
+//         // setTimeout(function(){
+//         // alert('alert Fired')
+//         // },3000)
+//         alert('alert Fired')
+//      alertTime=   setTimeout(startAlert, 2000)
+// }
+
+// function stopAlert(){
+//     clearInterval(alertTime)
+// }
